928c3062ca271cc10adcde23a99851a1dda275ff
bq-hentai <bqliu@iflytek.com>
2018-07-27 10:08:51 +0800

    add worker_loop

---
+++ mix/worker_loop/worker.js
@@ -0,0 +1,54 @@
+var results = []
+
+function resultReceiver(event) {
+  results.push(parseInt(event.data))
+  if (results.length == 2) {
+    postMessage(results[0] + results[1])
+  }
+}
+
+function errorReceiver(event) {
+  throw event.data
+}
+
+function sendReq() {
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', 'http://127.0.0.1:8080')
+  xhr.send()
+}
+
+var curDate = Date.now()
+
+/* loop 测试效果 */
+function polling() {
+  setInterval(function () {
+    var now = Date.now()
+
+    postMessage(now - curDate)
+
+    console.log('看看我能不能正常的打出来', now - curDate)
+
+    curDate = now
+
+    sendReq()
+  }, 2 * 1000)
+}
+
+polling()
+
+onmessage = function(event) {
+  var n = parseInt(event.data)
+
+  if (n == 0 || n == 1) {
+    postMessage(n)
+    return
+  }
+
+  for (var i = 1; i <= 2; i++) {
+    // 测试 worker 中再加  worker
+    // var worker = new Worker("fibonacci.js")
+    // worker.onmessage = resultReceiver
+    // worker.onerror = errorReceiver
+    postMessage(n - i);
+  }
+};
